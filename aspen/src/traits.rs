a29f80fdd979f1c50d3faf57c8274ecb97459cf0
Akshay <nerdy@peppe.rs>
2022-06-15 16:35:25 +0530

    add flat_map_capture

--- aspen/src/traits.rs
+++ aspen/src/traits.rs
@@ -2,6 +2,11 @@ use tree_sitter::{QueryCapture, QueryMatches, TextProvider};
 
 pub trait MapCapture {
     fn map_capture<B>(self, capture_name: &str, f: impl Fn(&QueryCapture) -> B) -> Vec<B>;
+    fn flat_map_capture<B>(
+        self,
+        capture_name: &str,
+        f: impl Fn(&QueryCapture) -> Option<B>,
+    ) -> Vec<B>;
 }
 
 impl<'a, 'tree, T> MapCapture for QueryMatches<'a, 'tree, T>
@@ -15,6 +20,20 @@ where
             .expect("u r idiot");
         self.flat_map(|m| m.captures.iter().filter(|c| c.index == capture_idx))
             .map(f)
+            .collect()
+    }
+
+    fn flat_map_capture<B>(
+        self,
+        capture_name: &str,
+        f: impl Fn(&QueryCapture) -> Option<B>,
+    ) -> Vec<B> {
+        let capture_idx = self
+            .query()
+            .capture_index_for_name(capture_name)
+            .expect("u r idiot");
+        self.flat_map(|m| m.captures.iter().filter(|c| c.index == capture_idx))
+            .flat_map(f)
             .collect()
     }
 }
