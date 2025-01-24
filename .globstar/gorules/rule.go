fc04f588334aaa297fb151cc1266ac1f70ac58a8
injuly <srijannr1@gmail.com>
2025-01-25 01:41:32 +0530

    go rules runner

---
+++ .globstar/gorules/rule.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/DeepSourceCorp/globstar/pkg/analysis"
+	sitter "github.com/smacker/go-tree-sitter"
+)
+
+func noDoubleEq(r analysis.Rule, ana *analysis.Analyzer, node *sitter.Node) {
+	op := node.ChildByFieldName("operator")
+	if op.Content(ana.ParseResult.Source) == "===" {
+		ana.Report(&analysis.Issue{
+			Message: "Do not use '==' for comparison. Prefer '===' instead.",
+			Range:   op.Range(),
+		})
+	}
+}
+
+func NoDoubleEq() analysis.Rule {
+	var entry analysis.VisitFn = noDoubleEq
+	return analysis.CreateRule("binary_expression", analysis.LangJs, &entry, nil)
+}
