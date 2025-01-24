df943c9888a4585022d8b22dc94396da7d9b7315
Sanket Saurav <sanket@deepsource.io>
2025-01-23 21:35:56 -0800

    feat: add Globstar animation and dark mode support

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,7 +1,14 @@
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
+import type { DefineComponent } from 'vue'
+import Layout from './Layout.vue'
+import GlobstarAnimation from './GlobstarAnimation.vue'
 import './style.css'
 
 export default {
-  extends: DefaultTheme
+  extends: DefaultTheme,
+  Layout: Layout as DefineComponent,
+  enhanceApp({ app }) {
+    app.component('GlobstarAnimation', GlobstarAnimation)
+  }
 } satisfies Theme
