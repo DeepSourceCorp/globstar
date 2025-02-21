e2b00dd0fa03b3540012ca30bc03ec5c2cc87f66
pkalyankumar1010 <pkalyankumar1010@gmail.com>
2025-02-21 20:24:04 +0530

    Adding no_inner_html test

--- checkers/javascript/no_inner_html.test.js
+++ checkers/javascript/no_inner_html.test.js
@@ -1,15 +1,27 @@
 
 
-// match
+// Match
+// <expect-error>
 document.body.innerHTML = `${name}`
+// <expect-error>
 document.body.innerHTML += document.getElementById("name").value;
+// <expect-error>
 document.querySelector("#div1").innerHTML += `${name}`;
+// <expect-error>
 document.querySelector("#div2").innerHTML += document.getElementById("name").value;
+// <expect-error>
 article.innerHTML += `${name}`;
+// <expect-error>
 article.innerHTML += document.getElementById("name").value;
+
 // Dont match 
+// <no-error>
 document.body.innerHTML += "<p>Hello</p>";
+// <no-error>
 document.querySelector("#div3").innerHTML = DOMPurify.sanitize(userInput);
+// <no-error>
 document.querySelector("#div4").innerHTML = DOMPurify.sanitize(document.getElementById("name").value);
+// <no-error>
 article.innerHTML = "<p>Hello</p>";
+// <no-error>
 article.innerHTML = DOMPurify.sanitize(userInput);
