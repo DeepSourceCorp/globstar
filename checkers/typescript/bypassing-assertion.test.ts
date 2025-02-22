71db64b00add96203b46f33d801f675dce05d20d
pkalyankumar1010 <pkalyankumar1010@gmail.com>
2025-02-22 23:04:22 +0530

    Add check for bypassing assertion and corrected test case file

--- checkers/typescript/bypassing-assertion.test.ts
+++ checkers/typescript/bypassing-assertion.test.ts
@@ -1,12 +1,10 @@
+
+interface Person {
+    name: string;
+    age: number;
+}
 // <expect-error>
-interface Person {
-    name: string;
-    age: number;
-}
 const person = { name: "Alice", age: 30, extra: true } as Person;
+
 // <no-error>
-interface Person {
-    name: string;
-    age: number;
-}
 const person : Person = { name: "Alice", age: 30, extra: true };
