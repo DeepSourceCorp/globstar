cf40ad17e0efa605d465265626d7f2399affcedd
pkalyankumar1010 <pkalyankumar1010@gmail.com>
2025-02-22 22:00:11 +0530

    Added no location.assign checker

--- checkers/javascript/location_assign.test.js
+++ checkers/javascript/location_assign.test.js
@@ -2,3 +2,7 @@
 location.assign(document.getElementById("input").value);
 // <expect-error>
 location.assign(new URLSearchParams(window.location.search).get("redirect"));
+// <expect-error>
+window.location.assign(document.getElementById("input").value);
+// <expect-error>
+window.location.assign(new URLSearchParams(window.location.search).get("redirect"));
