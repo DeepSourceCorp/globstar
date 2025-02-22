ef3ab85f421c9c5b84e95082896bff5c7afabc98
pkalyankumar1010 <pkalyankumar1010@gmail.com>
2025-02-23 00:14:38 +0530

    Removed more than 1 check

--- checkers/python/avoid-allowing-all-hosts.tests.py
+++ checkers/python/avoid-allowing-all-hosts.tests.py
@@ -1,7 +1,5 @@
 # Match 
 # <expect-error>
-DEBUG = True
-# <expect-error>
 ALLOWED_HOSTS = ["*"]
 # <expect-error>
 ALLOWED_HOSTS = ['*']
@@ -11,8 +9,7 @@ ALLOWED_HOSTS = ["google.com","*","facebook.com"]
 ALLOWED_HOSTS = ["*","google.com","facebook.com"]
 # <expect-error>
 ALLOWED_HOSTS = ["google.com","facebook.com","*"]
+
 # Not Macth
 # <no-error>
 ALLOWED_HOSTS = ["google.com","facebook.com"]
-# <no-error>
-DEBUG = False
