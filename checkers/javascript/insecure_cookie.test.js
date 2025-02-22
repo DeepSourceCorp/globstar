7fdbbabc70ec368e0ab3190f1c451ac190d3ffc3
pkalyankumar1010 <pkalyankumar1010@gmail.com>
2025-02-22 23:52:34 +0530

    Corrected the yml file and typo erros

--- checkers/javascript/insecure_cookie.test.js
+++ checkers/javascript/insecure_cookie.test.js
@@ -1,17 +1,17 @@
-// macth
-// <expext-error>
+// match
+// <expect-error>
 res.cookie('user', 'john_doe', { maxAge: 900000, httpOnly: false });
-// <expext-error>
+// <expect-error>
 res.cookie('user', 'john_doe', { httpOnly: false });
-// <expext-error>
+// <expect-error>
 res.cookie('user', 'john_doe', { httpOnly: false , maxAge: 900000, });
-// <expext-error>
+// <expect-error>
 res.cookie('user', 'john_doe', { secure : false });
-// <expext-error>
+// <expect-error>
 res.cookie('user', 'john_doe', { sameSite : false });
-// <expext-error>
+// <expect-error>
 res.cookie('user', 100, { sameSite : false });
-// <expext-error>
+// <expect-error>
 res.cookie('user', {my_name : "some_name"}, { sameSite : false });
 
 // Dont Match
