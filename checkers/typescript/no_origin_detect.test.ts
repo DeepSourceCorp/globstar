3f07b3212dcc77254c5bddfb2facd5d03a40baf1
pkalyankumar1010 <pkalyankumar1010@gmail.com>
2025-02-22 23:38:01 +0530

    Removed extra lines

--- checkers/typescript/no_origin_detect.test.ts
+++ checkers/typescript/no_origin_detect.test.ts
@@ -2,22 +2,12 @@
 // <expect-error>
 wss.on('connection', (ws : any, req: any) => {
     console.log('Browser connected to WebSocket server!');
-    const origin = req.headers.origin;
-
-    console.log(`Origin: ${origin}`);
-    
 })  
 // Dont Match
 // <no-errror>
 wss.on('connection', (ws : any, req: any) => {
-    console.log('Browser connected to WebSocket server!');
-
     const origin = req.headers.origin;
-
-    console.log(`Origin: ${origin}`);
-    
     if (origin !== "http://localhost:3000" && origin !== "https://www.youtube.com") {
-        console.log(`Connection rejected from origin: ${origin}`);
         ws.close(); 
         return;
     }
