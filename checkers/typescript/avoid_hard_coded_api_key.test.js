ead365186873f994adb4d357e2023495946a8a85
pkalyankumar1010 <pkalyankumar1010@gmail.com>
2025-02-23 14:38:34 +0530

    Add check for hard coded API key in Typescript

---
+++ checkers/typescript/avoid_hard_coded_api_key.test.js
@@ -0,0 +1,8 @@
+// Match
+// Source https://github.com/ethers-io/ethers.js/blob/main/src.ts/providers/provider-alchemy.ts#L40
+// <expect-error>
+const APIKEY = '4dac1ae64b623f1d33ae0b4ce0ff16a4';
+// <expect-error>
+let APIKEY = '4dac1ae64b623f1d33ae0b4ce0ff16a4';
+// <expect-error>
+let defaultApiKey = '4dac1ae64b623f1d33ae0b4ce0ff16a4';
