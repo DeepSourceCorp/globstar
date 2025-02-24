b3288d10cdd359bcb9d9a67c73dd3ec1591695a1
Utkarsh Singh <utkarsh190601@gmail.com>
2025-02-24 09:05:47 +0530

    fix: match the query in the context of func

--- checkers/python/exposed-stacktrace.test.py
+++ checkers/python/exposed-stacktrace.test.py
@@ -1,9 +1,19 @@
+import logging
 import traceback
 
 from django.http import HttpResponse
 
+LOGGER = logging.getLogger(__name__)
 
-def debug_view():
+
+# <expect-error>
+def debug_view(request):
     error_trace = traceback.format_exc()
-    # <expect-error>
     return HttpResponse(error_trace, content_type="text/plain", status=500)
+
+
+# <no-error>
+def save_view(request):
+    tb = traceback.format_stack()
+    LOGGER.error(tb)
+    return HttpResponse("Internal Server Error", content_type="text/plain", status=500)
