8081643ccb5cc664bbb6b3eee0d6e0b2dbbf9691
Maharshi Basu <basumaharshi10@gmail.com>
2025-02-26 10:18:38 +0530

    chore: update checker message and add reference to test file
    Signed-off-by: Maharshi Basu <basumaharshi10@gmail.com>

--- checkers/python/jwt-python-none-alg.test.py
+++ checkers/python/jwt-python-none-alg.test.py
@@ -1,9 +1,13 @@
 import jwt
 
+# adapted from
+# - https://github.com/Shopify/shopify_python_api/blob/main/test/session_token_test.py#L59
+# - https://github.com/flipkart-incubator/Astra/blob/master/modules/jwt_attack.py#L37
 def bad1():
     # <expect-error>
     encoded = jwt.encode({'some': 'payload'}, None, algorithm='none')
     return encoded
+
 
 def bad2(encoded):
     # <expect-error>
