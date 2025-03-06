1ab7b92d213534390a0bf932d37db21f1eadae73
pkalyankumar1010 <pkalyankumar1010@gmail.com>
2025-03-06 19:38:24 +0530

    Corrected descrption and query

--- checkers/typescript/avoid_using_any_type.test.ts
+++ checkers/typescript/avoid_using_any_type.test.ts
@@ -1,4 +1,6 @@
 // <expect-error>
-let data: any = "Hello";
+let data1: any = "Hello";
 // <no-error>
-let data: string = "Hello";
+let data2: string = "Hello";
+// <no-error>
+let data3: unknown = "Hello";
