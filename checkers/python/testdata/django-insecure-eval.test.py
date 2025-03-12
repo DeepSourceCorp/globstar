e234e630134767652dd48997ffb05051a676b70f
Maharshi Basu <basumaharshi10@gmail.com>
2025-03-12 21:33:12 +0530

    checker(python): Add rules to detect user data in eval through formatted strings
    Signed-off-by: Maharshi Basu <basumaharshi10@gmail.com>

--- checkers/python/testdata/django-insecure-eval.test.py
+++ checkers/python/testdata/django-insecure-eval.test.py
@@ -25,3 +25,76 @@ def safe(request):
     print('hello')
     """
     eval(dedent(code_safe))
+
+def unsafe_percent_formatting(request):
+    message = request.POST.get('message')
+    print("do stuff here")
+    code_percent_fmt = """
+    print(%s)
+    """ % message
+    # <expect-error>
+    eval(code_percent_fmt)
+
+def unsafe_inline(request):
+    # <expect-error>
+    eval("print(%s)" % request.GET.get('message'))
+
+def unsafe_dict(request):
+    # <expect-error>
+    eval("print(%s)" % request.POST['message'])
+
+def safe(request):
+    code = """
+    print('hello')
+    """
+    # <no-error>
+    eval(dedent(code))
+
+def fmt_unsafe(request):
+    message = request.POST.get('message')
+    print("do stuff here")
+    code_fmt_call = """
+    print({})
+    """.format(message)
+    # <expect-error>
+    eval(code_fmt_call)
+
+def fmt_unsafe_inline(request):
+    # <expect-error>
+    eval("print({})".format(request.GET.get('message')))
+
+def fmt_unsafe_dict(request):
+    # <expect-error>
+    eval("print({}, {})".format(request.POST['message'], "pwned"))
+
+def fmt_safe(request):
+    code = """
+    print('hello')
+    """
+    # <no-error>
+    eval(dedent(code))
+
+def fstr_unsafe(request):
+    message = request.POST.get('message')
+    print("do stuff here")
+    code_unsafe_fstring = f"""
+    print({message})
+    """
+    # <expect-error>
+    eval(code_unsafe_fstring)
+
+def fstr_unsafe_inline(request):
+    # <expect-error>
+    eval(f"print({request.GET.get('message')})")
+
+def fstr_unsafe_dict(request):
+    # <expect-error>
+    eval(f"print({request.POST['message']})")
+
+def fstr_safe(request):
+    var = "hello"
+    code = f"""
+    print('{var}')
+    """
+    # <no-error>
+    eval(dedent(code))
