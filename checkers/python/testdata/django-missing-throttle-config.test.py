34dc5bc93da53ddc6a2ba31615bdf4ab9249f966
Maharshi Basu <basumaharshi10@gmail.com>
2025-03-17 11:51:53 +0530

    checkers(python): add checker to detect missing throttle config in django REST framework
    Signed-off-by: Maharshi Basu <basumaharshi10@gmail.com>

---
+++ checkers/python/testdata/django-missing-throttle-config.test.py
@@ -0,0 +1,19 @@
+# <no-error>
+REST_FRAMEWORK = {
+    'PAGE_SIZE': 10,
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/day',
+        'user': '1000/day'
+    },
+    "SOMETHING_ELSE": {1: 2}
+}
+
+# <expect-error>
+REST_FRAMEWORK = {
+    'PAGE_SIZE': 10
+}
+
