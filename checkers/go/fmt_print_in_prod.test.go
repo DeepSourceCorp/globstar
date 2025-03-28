c7c7d4a90a84c01178debabb896f741695cb8678
Sourya Vatsyayan <sourya@deepsource.io>
2025-03-28 11:57:59 +0530

    chore: cleanup go checkers tests
    Signed-off-by: Sourya Vatsyayan <sourya@deepsource.io>

--- checkers/go/fmt_print_in_prod.test.go
+++ checkers/go/fmt_print_in_prod.test.go
@@ -1,31 +1,24 @@
-// <expect-error>
-fmt.Print()
-
-// <expect-error>
-fmt.Println()
-
-
-package main
+package golang
 
 import (
 	"fmt"
-	"logger"
+	"log"
 )
 
-func main() {
+func testFmtInProd() {
 	// <expect-error>
-    fmt.Print("Hello, ")
+	fmt.Print("Hello, ")
 	// <expect-error>
-    fmt.Print("World!")
+	fmt.Print("World!")
 	// <expect-error>
-	fmt.Println("Hello, World!") 
+	fmt.Println("Hello, World!")
 	// <expect-error>
-    fmt.Println("Line 1")
+	fmt.Println("Line 1")
 	// <expect-error>
 	fmt.Printf("Name: %s, Age: %d\n", name, age)
 
 	// Safe
-	logger.Info("Hello, World!")
+	log.Println("Hello, World!")
 	//Safe
-	logger.Debug("Hello, World!")
+	log.Println("Hello, World!")
 }
