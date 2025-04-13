722edd39b55fea8157d35688276789be8c890835
Unnat Sharma <officialunnat30@gmail.com>
2025-04-13 16:54:58 +0530

    chore(checkers/js): Update the `js_lazy_load_module` checker to correctly identify `require()` calls within arrow function bodies. Additionaly, ensure that direct assignments inside function to `require` are also flagged.

--- checkers/javascript/js_lazy_load_module.test.js
+++ checkers/javascript/js_lazy_load_module.test.js
@@ -2,16 +2,37 @@
 const fs = require('fs')
 
 function smth() {
-  // <expect-error>
-  const mod = require('module-name');
-  return mod();
+    // <expect-error>
+    const mod = require('module-name');
+    return mod();
 }
 
 function smth2(){
     console.log("Hello");
     // <expect-error>
-    let mod = require('some-module');
+    mod = require('some-module');
     return mod.doSomn();
 }
 
+const arrowFunction = () => {
+    //<expect-error>
+    const someModule = require('some-module');
+    return someModule.doSomn();
+}
 
+
+const expressionFunction = function() {
+    //<expect-error> 
+    const anotherModule = require('some-module');
+    return anotherModule.doSomething();
+};
+
+
+function outerFunction() {
+    function innerFunction() {
+        //<expect-error>
+        const yetAnotherModule = require('some-module');
+        return yetAnotherModule.doSomething();
+    }
+  return innerFunction();
+}
